fda7106107c4615dfe0758bd9c2e28347dc8d509
evenmare <kir9ver@gmail..com>
2021-07-24 11:54:23 +0300

    added new urls

--- jobs_project/urls.py
+++ jobs_project/urls.py
@@ -14,13 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth.views import LogoutView
 from job_app.views import MainView, VacanciesView, CompanyView, SpecializedVacanciesView, \
-    VacancyView, CompanyLetsStart, CompanyEdit, CompanyCreate, CompanyVacancies, \
-    CompanyVacancyCreate, error404_view, error500_view, CompanyVacancyEdit
+    VacancyView, CompanyLetsStartView, CompanyEditView, CompanyCreateView, CompanyVacanciesView, \
+    CompanyVacancyCreateView, error404_view, error500_view, CompanyVacancyEditView, ResumeCreateView, ResumeEditView
 from authentifiaction_app.views import SignupView, UserLoginView
 
 handler404 = error404_view
@@ -32,12 +33,14 @@ urlpatterns = [
     path('login/', UserLoginView.as_view(), name='login'),
     path('register/', SignupView.as_view(), name='sign_up'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('mycompany/letsstart/', CompanyLetsStart.as_view(), name='company_letsstart'),
-    path('mycompany/create/', CompanyCreate.as_view(), name='company_create'),
-    path('mycompany/', CompanyEdit.as_view(), name='company_edit'),
-    path('mycompany/vacancies', CompanyVacancies.as_view(), name='company_vacancies'),
-    path('mycompany/vacancies/create', CompanyVacancyCreate.as_view(), name='company_vacancy_create'),
-    path('mycompany/vacancies/<int:pk>', CompanyVacancyEdit.as_view(), name='company_vacancy_edit'),
+    path('mycompany/letsstart/', login_required(CompanyLetsStartView.as_view()), name='company_letsstart'),
+    path('mycompany/create/', login_required(CompanyCreateView.as_view()), name='company_create'),
+    path('mycompany/', login_required(CompanyEditView.as_view()), name='company_edit'),
+    path('mycompany/vacancies', login_required(CompanyVacanciesView.as_view()), name='company_vacancies'),
+    path('mycompany/vacancies/create', login_required(CompanyVacancyCreateView.as_view()), name='company_vacancy_create'),
+    path('mycompany/vacancies/<int:pk>', login_required(CompanyVacancyEditView.as_view()), name='company_vacancy_edit'),
+    path('myresume/create', login_required(ResumeCreateView.as_view()), name='resume_create'),
+    path('myresume/', login_required(ResumeEditView.as_view()), name='resume_edit'),
     path('vacancies/', VacanciesView.as_view(), name='vacancies'),
     path('vacancies/cat/<str:category>/', SpecializedVacanciesView.as_view(), name='categorized_vacancies'),
     path('vacancies/<int:pk>/', VacancyView.as_view(), name='vacancy'),
