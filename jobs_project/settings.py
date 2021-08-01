c5730076ae893c0a9870802f8149c74c56aea248
evenmare <kir9ver@gmail..com>
2021-08-01 19:14:24 +0300

    minors

--- jobs_project/settings.py
+++ jobs_project/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
     'crispy_forms',
     'taggit',
     'phonenumber_field',
